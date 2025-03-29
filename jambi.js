2352cf74d87931f9d3d4bfb8fbf8510e579c7e56
Ken Kapptie <kkapptie@graniteschools.org>
2025-03-29 08:11:48 -0600

    Update jambi.js

--- jambi.js
+++ jambi.js
@@ -7,7 +7,7 @@ function loadJambi() {
         measures: 16,
         rootNote: "E",
         mode: "Phrygian",
-        tempo: 120,
+        tempo: 93,
         timeSignature: "9/8",
         feel: "Mystical",
         lyrics: "Here from the king's mountain view\nHere from the wild dream come true\nFeast like a sultan I do\nOn treasures and flesh, never few"
@@ -17,7 +17,7 @@ function loadJambi() {
         measures: 24,
         rootNote: "E",
         mode: "Phrygian",
-        tempo: 120,
+        tempo: 93,
         timeSignature: "9/8",
         feel: "Tension",
         lyrics: "The devil and his had me down\nIn love with the dark side I'd found\nDabble in all the way down\nUp to my neck soon to drown"
@@ -27,7 +27,7 @@ function loadJambi() {
         measures: 24,
         rootNote: "E",
         mode: "Phrygian",
-        tempo: 120,
+        tempo: 93,
         timeSignature: "9/8",
         feel: "Triumph",
         lyrics: "But I, I would wish it all away\nIf I thought I'd lose you just one day"
@@ -37,7 +37,7 @@ function loadJambi() {
         measures: 24,
         rootNote: "E",
         mode: "Phrygian",
-        tempo: 120,
+        tempo: 93,
         timeSignature: "9/8",
         feel: "Tension",
         lyrics: "Prayed like a martyr dusk to dawn\nBegged like a hooker all night long\nTempted the devil with my song\nAnd got what I wanted all along"
@@ -47,7 +47,7 @@ function loadJambi() {
         measures: 24,
         rootNote: "E",
         mode: "Phrygian",
-        tempo: 120,
+        tempo: 93,
         timeSignature: "9/8",
         feel: "Triumph",
         lyrics: "But I, and I would\nIf I could, and I would\nWish it away, wish it away\nWish it all away"
@@ -57,7 +57,7 @@ function loadJambi() {
         measures: 32,
         rootNote: "E",
         mode: "Phrygian",
-        tempo: 120,
+        tempo: 93,
         timeSignature: "6/4",
         feel: "Intense",
         lyrics: "[Guitar solo with talk box effect]"
@@ -67,7 +67,7 @@ function loadJambi() {
         measures: 16,
         rootNote: "E",
         mode: "Phrygian",
-        tempo: 120,
+        tempo: 93,
         timeSignature: "9/8",
         feel: "Mystical",
         lyrics: "Shine on, benevolent sun\nShine down upon the broken"
@@ -77,7 +77,7 @@ function loadJambi() {
         measures: 24,
         rootNote: "E",
         mode: "Phrygian",
-        tempo: 120,
+        tempo: 93,
         timeSignature: "9/8",
         feel: "Triumph",
         lyrics: "Shine until the two become one\nBreathe in union"
@@ -87,7 +87,7 @@ function loadJambi() {
         measures: 16,
         rootNote: "E",
         mode: "Phrygian",
-        tempo: 120,
+        tempo: 93,
         timeSignature: "9/8",
         feel: "Mystical",
         lyrics: "[Psychedelic trail off]"
