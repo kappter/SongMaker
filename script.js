f9d6c1fc83a707d1b5044afdfd5ee90540ad63bf
Ken Kapptie <kkapptie@graniteschools.org>
2025-04-04 12:14:26 -0600

    Update script.js

--- script.js
+++ script.js
@@ -874,17 +874,19 @@ function printSong() {
   // Store original content to restore later
   const originalContent = currentBlockDisplay.innerHTML;
 
-  // Populate current-block-display with song info and copyright
+  // Populate with concise info, add print-specific class
+  currentBlockDisplay.classList.add('print-view');
   currentBlockDisplay.innerHTML = `
     <span class="label">
-      Total Duration: ${formatDuration(totalSeconds)} | Beats: ${totalBeats} | Blocks: ${blockCount}<br>
-      © 2025 SongMaker by kappter. All rights reserved.
+      ${currentSongName} | ${formatDuration(totalSeconds)} | ${totalBeats} Beats | ${blockCount} Blocks
+      <br>© 2025 SongMaker by kappter
     </span>
   `;
 
   window.print();
 
-  // Restore original content after printing
+  // Restore original state
+  currentBlockDisplay.classList.remove('print-view');
   currentBlockDisplay.innerHTML = originalContent;
 }
 
